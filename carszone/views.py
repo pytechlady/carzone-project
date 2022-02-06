640777606ddc889640937fdf7bedbf876c8427d3
newbietechlady <fumex9910@gmail.com>
2022-02-06 20:21:19 +0100

    template and static features

--- carszone/views.py
+++ carszone/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
 
+import carszone
+
 # Create your views here.
 def home(request):
     return render(request, 'carszone/home.html')
+
+def about(request):
+    return render(request, 'carszone/about.html')
+
+def services(request):
+    return render(request, 'carszone/services.html')
+
+def contact(request):
+    return render(request, 'carszone/contact.html')
